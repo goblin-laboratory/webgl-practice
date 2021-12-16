9698e8fe286272e38fe4b36b147664c2e96ce699
xiaosong <kunkkaco@gmail.com>
2021-12-16 18:20:14 +0800

    feat: 初识 WebGL :sparkles:
    https://developer.mozilla.org/zh-CN/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL

--- src/containers/MDNGettingStarted/index.js
+++ src/containers/MDNGettingStarted/index.js
@@ -1,8 +1,33 @@
 import React from "react";
+import { Modal } from "antd";
 // import './App.css';
 
 function MDNGettingStarted() {
-  return <div className="MDN">MDN Getting started with WebGL</div>;
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    const gl = ref.current.getContext("webgl");
+    // 确认WebGL支持性
+    if (!gl) {
+      Modal.error({
+        title: "无法初始化WebGL",
+        content: "你的浏览器、操作系统或硬件等可能不支持WebGL。",
+      });
+      return;
+    }
+    // 使用完全不透明的黑色清除所有图像
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // 用上面指定的颜色清除缓冲区
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={ref} id="glcanvas" width="640" height="480">
+        你的浏览器似乎不支持或者禁用了HTML5 <code>&lt;canvas&gt;</code> 元素.
+      </canvas>
+    </div>
+  );
 }
 
 export default React.memo(MDNGettingStarted, () => true);
