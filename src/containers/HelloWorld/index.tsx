21c0e81a1e72ad131c9134809149a43158951fc0
xiaosong <kunkkaco@gmail.com>
2023-03-21 23:04:40 +0800

    feat: 绘制旋转立方体 :new:

---
+++ src/containers/HelloWorld/index.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { WebGLRenderer, PerspectiveCamera, Scene, DirectionalLight, BoxGeometry, MeshPhongMaterial, Mesh } from 'three';
+
+function initRenderer(canvas:any) {
+  const renderer = new WebGLRenderer({antialias: true, canvas});
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  return renderer;
+}
+
+function initCamera(){
+  const fov = 75;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 10;
+  return camera;
+}
+
+let initialState = false;
+function init(canvas?:any){
+  if (!canvas || initialState) {
+    return;
+  }
+  initialState = true;
+  const renderer = initRenderer(canvas);
+  const camera = initCamera();
+
+  const scene = new Scene();
+
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  const material = new MeshPhongMaterial({color: 0x44aa88});  // greenish blue
+
+  const cube = new Mesh(geometry, material);
+  scene.add(cube);
+
+  function render(time:any) {
+    time *= 0.001;  // convert time to seconds
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
+
+function HelloWorld() {
+  const ref = React.useRef<any>();
+  React.useEffect(() => {
+    init(ref.current);
+  }, []);
+  return (
+    <canvas ref={ref} style={{width: '100%', height: '100%', display: "block"}} />
+  );
+}
+
+export default HelloWorld;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import HelloWorld from "./containers/HelloWorld";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
